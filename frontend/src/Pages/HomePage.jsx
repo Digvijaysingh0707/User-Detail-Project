aa0da293a64730fcec8a641c120cabce6cdadbfb
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2024-05-08 01:36:08 +0530

    initial commit

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserDetailsForm from '../components/UserDetailsForm'
+
+const HomePage = () => {
+  return (
+    <div>
+      <UserDetailsForm />
+    </div>
+  )
+}
+
+export default HomePage
